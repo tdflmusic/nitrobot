5c19541fdfd9cbf2ff1a19eee98a1e611787882b
tdflmusic <41844918+tdflmusic@users.noreply.github.com>
2018-07-29 03:48:56 +0200

    H
    ye

---
+++ bot.js
@@ -0,0 +1,31 @@
+const botSettings = require("./botsettings.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+const embed = new Discord.RichEmbed()
+  .setTitle("Oh no!")
+
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x8080ff)
+  .setDescription("You need [Discord Nitro](https://discordapp.com/nitro) to view this message!")
+  .setThumbnail("https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png");
+
+	
+
+bot.on('ready', () => {
+    console.log("I am ready");
+});
+ 
+bot.on('message', async message => {
+    if(message.author !== bot.user) return;
+ 
+    if(message.content === "!(!)!") {
+    	message.delete(10)
+        message.channel.send({embed});
+    }
+});
+
+bot.login(botSettings.token);
